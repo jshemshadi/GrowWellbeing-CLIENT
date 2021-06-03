72a0216faeb7d2e0b019aa5e3d927d10c144fce4
jshemshadi <jshemshadi@gmail.com>
2021-06-03 21:43:53 +0430

    Update

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 
 import Appointments from "../screens/appointments";
 import Dashboard from "../screens/dashboard";
-import Users from "../screens/users";
 import Messages from "../screens/messages";
 import Notifications from "../screens/notifications";
 import Profile from "../screens/profile";
@@ -15,7 +14,6 @@ const Routes = () => (
       <Route exact path="/profile" component={Profile} />
       <Route exact path="/myAccount" component={MyAccount} />
       <Route exact path="/dashboard" component={Dashboard} />
-      <Route exact path="/users" component={Users} />
       <Route exact path="/appointments" component={Appointments} />
       <Route exact path="/notifications" component={Notifications} />
       <Route exact path="/messages" component={Messages} />
